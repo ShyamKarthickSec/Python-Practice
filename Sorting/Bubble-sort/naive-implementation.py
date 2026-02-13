bcede9f28a55e8aedfaed501aa6be4deb077042d
shyam karthick <shyamkarthick20@gmail.com>
2026-02-13 15:03:14 +1100

    Bubble Sort

---
+++ Sorting/Bubble-sort/naive-implementation.py
@@ -0,0 +1,11 @@
+l = [23,11,2,3,18]
+
+def bubblesort(l):
+    n = len(l)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if l[j] > l[j+1]:
+                l[j], l[j+1] = l[j+1], l[j]
+    return l
+
+print(bubblesort(l))
